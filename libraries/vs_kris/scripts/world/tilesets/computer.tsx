29b8efa54a1a9cb7aef81c480d8b271a5e8becee
Attila135 <magyarosia.25ad@acsjszki.hu>
2025-09-23 22:39:39 +0200

    clean up

--- libraries/vs_kris/scripts/world/tilesets/computer.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="computer" tilewidth="92" tileheight="56" tilecount="1" columns="1">
- <editorsettings>
-  <export target="computer.lua" format="lua"/>
- </editorsettings>
- <image source="../../../assets/sprites/tilesets/computer.png" width="92" height="56"/>
-</tileset>
--- libraries/vs_kris/scripts/world/tilesets/school.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="school" tilewidth="40" tileheight="40" tilecount="108" columns="9">
- <image source="../../../assets/sprites/tilesets/school/bg_schooltiles.png" width="360" height="480"/>
-</tileset>
