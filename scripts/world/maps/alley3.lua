559af3453094698ee9ebbfbbbb65af04edbdb355
Attila135 <magyarosia.25ad@acsjszki.hu>
2025-11-21 21:46:52 +0100

    alley3 rework

--- scripts/world/maps/alley3.lua
+++ scripts/world/maps/alley3.lua
@@ -5,12 +5,12 @@ return {
   class = "",
   orientation = "orthogonal",
   renderorder = "right-down",
-  width = 27,
-  height = 14,
+  width = 20,
+  height = 35,
   tilewidth = 40,
   tileheight = 40,
   nextlayerid = 13,
-  nextobjectid = 105,
+  nextobjectid = 214,
   properties = {},
   tilesets = {
     {
@@ -40,8 +40,8 @@ return {
       type = "tilelayer",
       x = 0,
       y = 0,
-      width = 27,
-      height = 14,
+      width = 20,
+      height = 35,
       id = 1,
       name = "Tile Layer 1",
       class = "",
@@ -54,20 +54,41 @@ return {
       properties = {},
       encoding = "lua",
       data = {
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293,
-        293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293, 293
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 543, 543, 543, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 541, 542, 543, 544, 545, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 581, 582, 581, 582, 583, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 594, 595, 594, 595, 596, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 594, 595, 594, 595, 596, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 568, 569, 570, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 568, 569, 570, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 568, 569, 570, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 568, 569, 570, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 581, 582, 583, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 594, 595, 596, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 327, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 500, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 50, 0, 0, 553, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 577, 0, 0, 0, 0, 0, 0, 0, 0, 79, 0, 0, 0, 0, 0,
+        0, 0, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 31, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 376, 0, 0, 0, 0, 95, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 661, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 380, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 659, 0, 0, 0, 0, 0, 0, 0,
+        0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 158, 0, 0, 0, 0, 0, 0, 0, 13, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 283, 0, 0, 0, 0, 0, 410, 0, 0, 0,
+        0, 0, 440, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
       }
     },
     {
@@ -85,30 +106,237 @@ return {
       properties = {},
       objects = {
         {
-          id = 102,
+          id = 121,
           name = "",
           type = "",
-          shape = "polygon",
-          x = 0,
-          y = 240,
-          width = 0,
-          height = 0,
+          shape = "rectangle",
+          x = 800,
+          y = 280,
+          width = 40,
+          height = 1120,
           rotation = 0,
           visible = true,
-          polygon = {
-            { x = 0, y = 0 },
-            { x = 280, y = -40 },
-            { x = 1080, y = -40 },
-            { x = 1080, y = 120 },
-            { x = 280, y = 120 },
-            { x = 0, y = 80 },
-            { x = 0, y = 320 },
-            { x = 1080, y = 320 },
-            { x = 1160, y = 320 },
-            { x = 1160, y = -240 },
-            { x = 120, y = -240 },
-            { x = 0, y = -240 }
-          },
+          properties = {}
+        },
+        {
+          id = 123,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = -40,
+          y = 280,
+          width = 40,
+          height = 1120,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 124,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 0,
+          y = 1400,
+          width = 320,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 125,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 480,
+          y = 1400,
+          width = 320,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 137,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 440,
+          y = 520,
+          width = 360,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 138,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 0,
+          y = 520,
+          width = 320,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 139,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 280,
+          y = 400,
+          width = 40,
+          height = 120,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 141,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 440,
+          y = 400,
+          width = 40,
+          height = 120,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 142,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 240,
+          y = 280,
+          width = 40,
+          height = 120,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 143,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 480,
+          y = 280,
+          width = 40,
+          height = 120,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 144,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 0,
+          y = 160,
+          width = 800,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 159,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = -40,
+          y = 200,
+          width = 40,
+          height = 80,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 161,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 240,
+          y = 240,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 162,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 480,
+          y = 240,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 163,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 280,
+          y = 200,
+          width = 200,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 164,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 800,
+          y = 200,
+          width = 40,
+          height = 80,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 167,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 320,
+          y = 1400,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 168,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 440,
+          y = 1400,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
           properties = {}
         }
       }
@@ -177,8 +405,8 @@ return {
           name = "spawn",
           type = "",
           shape = "point",
-          x = 40,
-          y = 280,
+          x = 402,
+          y = 1368,
           width = 0,
           height = 0,
           rotation = 0,
@@ -206,32 +434,62 @@ return {
           name = "transition",
           type = "",
           shape = "polygon",
-          x = -40,
-          y = 240,
+          x = 395,
+          y = 1485,
           width = 40,
           height = 80,
           rotation = 0,
           visible = true,
           polygon = {
-            { x = -10, y = -40 },
-            { x = 40, y = 0 },
-            { x = 40, y = 80 },
-            { x = -10, y = 40 }
+            { x = -35, y = -85 },
+            { x = 45, y = -85 },
+            { x = 45, y = -45 },
+            { x = -35, y = -45 }
           },
           properties = {
-            ["map"] = "alley",
+            ["map"] = "alley3",
             ["marker"] = "spawn"
           }
         },
         {
-          id = 103,
+          id = 104,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 360,
+          y = 280,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 155,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 755,
+          y = 1102,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 165,
           name = "npc",
           type = "",
           shape = "rectangle",
-          x = 1019.26,
-          y = 259.506,
-          width = 54.6667,
-          height = 45.3679,
+          x = 360,
+          y = 360,
+          width = 40,
+          height = 40,
           rotation = 0,
           visible = true,
           properties = {
@@ -240,12 +498,267 @@ return {
           }
         },
         {
-          id = 104,
+          id = 171,
           name = "blocktree",
           type = "",
           shape = "rectangle",
-          x = 740,
-          y = 160,
+          x = -91,
+          y = 1171,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 172,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = -13,
+          y = 1297,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 174,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 726,
+          y = 1430,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 181,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 585,
+          y = 1510,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 183,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = -99,
+          y = 1361,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 186,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = -31,
+          y = 1066,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 187,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = -112,
+          y = 891,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 190,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = -65,
+          y = 770,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 196,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 816,
+          y = 1325,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 197,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 871,
+          y = 1194,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 200,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 856,
+          y = 953,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 203,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 835,
+          y = 807,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 206,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 401,
+          y = 1492,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 209,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 179,
+          y = 1511,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 210,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = -16,
+          y = 1480,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 211,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 214,
+          y = 1202,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 212,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 605,
+          y = 967,
+          width = 40,
+          height = 40,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["flagcheck"] = "!man_room"
+          }
+        },
+        {
+          id = 213,
+          name = "blocktree",
+          type = "",
+          shape = "rectangle",
+          x = 124,
+          y = 941,
           width = 40,
           height = 40,
           rotation = 0,
