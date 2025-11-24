902c7cc951a5b72f92982624bf8bcbfc90c6b0a4
Attila135 <magyarosia.25ad@acsjszki.hu>
2025-11-24 17:44:27 +0100

    Update party.lua

--- scripts/world/cutscenes/party.lua
+++ scripts/world/cutscenes/party.lua
@@ -1,9 +1,31 @@
 return {
     delta = function(cutscene, event)
+        local kris = cutscene:getCharacter("kris")
+        local susie = cutscene:getCharacter("susie")
+        local ralsei = cutscene:getCharacter("ralsei")
 
+        if kris and susie and ralsei then
+            Game:addPartyMember("vessel")
+            Game:addPartyMember("noelle")
+            Game:addPartyMember("watercooler")
+            Game:removePartyMember("kris")
+            Game:removePartyMember("susie")
+            Game:removePartyMember("ralsei")
+        end
     end,
 
     nabla = function(cutscene, event)
+        local vessel = cutscene:getCharacter("vessel")
+        local noelle = cutscene:getCharacter("noelle")
+        local watercooler = cutscene:getCharacter("watercooler")
 
+        if vessel and noelle and watercooler then
+            Game:addPartyMember("kris")
+            Game:addPartyMember("susie")
+            Game:addPartyMember("ralsei")
+            Game:removePartyMember("vessel")
+            Game:removePartyMember("noelle")
+            Game:removePartyMember("watercooler")
+        end
     end
 }
